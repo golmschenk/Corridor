694e77d61efc1f128a1deffb7e7c9dcba8fe3cf6
Greg Olmschenk <golmschenk@gmail.com>
2015-01-29 11:24:51 -0500

    Initial commit

---
+++ Corridor/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Corridor
+//
+//  Created by Greg Olmschenk on 1/29/15.
+//  Copyright (c) 2015 Greg Olmschenk. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
