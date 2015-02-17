ebd891540a7cebfd7f3526c61bae6258a4523638
Greg Olmschenk <golmschenk@gmail.com>
2015-02-17 17:28:33 -0500

    Add test images

--- CorridorTests/ViewControllerTests.swift
+++ CorridorTests/ViewControllerTests.swift
@@ -48,16 +48,4 @@ class CorridorTests: XCTestCase {
         XCTAssertTrue(viewController.respondsToSelector(Selector("processFrameButton")))
     }
     
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        XCTAssert(true, "Pass")
-//    }
-//
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measureBlock() {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-    
 }
